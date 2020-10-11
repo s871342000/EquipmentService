245a2db8f3da8be6475d650326a5556a7c31a601
s871342000 <s871342000@gmail.com>
2020-10-11 18:50:24 +0800

    temp

--- ClientApp/src/app/layout/App.tsx
+++ ClientApp/src/app/layout/App.tsx
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from "../../features/Login"
 import Device from "../../features/DeviceDetail"
 
 export default class App extends Component {
   render() {
-    return ({ Home }
-      /*<Router>
+    return (//{ Home }
+      <Router>
         <Route exact path="/" component={Home} />
         <Route path="/DeviceDetail" component={Device} />
-      </Router>*/
+      </Router>
     );
   }
 }
