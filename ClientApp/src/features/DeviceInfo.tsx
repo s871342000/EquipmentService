10891e162cfb05b4a92ac30a62ee30f8a19e2b4c
s871342000 <s871342000@gmail.com>
2020-10-12 07:20:44 +0800

    temp

--- ClientApp/src/features/DeviceInfo.tsx
+++ ClientApp/src/features/DeviceInfo.tsx
@@ -17,7 +17,7 @@ export const DeviceInfo = (props: { login: boolean }) => {
   let getHost = () => {
     if (typeof window !== 'undefined') {
       qrcodeUrl = window.location.protocol + '//' + window.location.host;
-      qrcodeUrl += "//EquipmentService//DeviceDetail";
+      // qrcodeUrl += "//EquipmentService//DeviceDetail";
     }
   }
 
