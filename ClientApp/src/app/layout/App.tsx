593e43e857ae58780eab821009d49e2c66c0b3f6
s871342000 <s871342000@gmail.com>
2020-10-11 18:22:33 +0800

    temp

--- ClientApp/src/app/layout/App.tsx
+++ ClientApp/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from "../../features/Login"
 import Device from "../../features/DeviceDetail"
 
